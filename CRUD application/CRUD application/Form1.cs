239a20c13bf841b4835681c95d79562feec2d8cb
Ahmed Sadek <a7medhanysadek@gmail.com>
2024-02-29 20:07:22 +0200

    adding oop concepts to CRUD application project

--- CRUD application/CRUD application/Form1.cs
+++ CRUD application/CRUD application/Form1.cs
@@ -2,10 +2,45 @@ using System.Collections.Specialized;
 
 namespace CRUD_application
 {
+    public class database
+    {
+        
+        List<string> l;
+        public database()
+        {
+            l = new List<string>();
+        }
+        public int size()
+        {
+            return l.Count;
+        }
+        public void create(string s)
+        {
+            l.Add(s);
+        }
+        public string read_by_index(int i)
+        {
+            return l.ElementAt(i);
+        }
+        public bool is_exist(string element)
+        {
+            return l.Contains(element);
+        }
+        public void update(string original_value,string new_value)
+        {
+            int index = l.IndexOf(original_value);
+            l.Remove(original_value);
+            l.Insert(index, new_value);
+        }
+        public void delete(string element)
+        {
+            l.Remove(element);
+        }
+    }
     public partial class Form1 : Form
     {
-        List<string> l;
         string list;
+        database d;
         public Form1()
         {
             InitializeComponent();
@@ -13,7 +48,7 @@ namespace CRUD_application
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            l = new List<string>();
+            d =new database();
         }
         //åßÊÈ ÈÇáÚÑÈí ÈÓ áÇä áæ ßÊÈÊ ãÚÇå ÈÇáÇäÌáíÒí ÇáßáÇã åíÏÎá Ýí ÈÚÖå
         //ÇáÝäßÔä Çááí ÊÍÊ Ïí æÙíÝÊåÇ ÊÎÝí ÇáÇÒÑÇÑ ÇáÇÓÇÓíå (ÇáßÑæÏ) æÈÊÙåÑ ÒÑÇÑ ãåã áÇä ãä ÎáÇáå äÞÏÑ äÑÌÚ ááÇÒÑÇÑ ÇáÇÓÇÓíÉ Çááí åí ÇáßÑæÏ
@@ -62,7 +97,7 @@ namespace CRUD_application
         {
             if (create_txtbox.Text != string.Empty)
             {
-                l.Add(create_txtbox.Text);
+                d.create(create_txtbox.Text);
                 MessageBox.Show("the dataset has been created");
                 create_txtbox.Text = string.Empty;
             }
@@ -98,11 +133,11 @@ namespace CRUD_application
                 MessageBox.Show("invalid index");
             else
             {
-                if (int.Parse(read_txtbox.Text) >= l.Count() || int.Parse(read_txtbox.Text) < 0)
+                if (int.Parse(read_txtbox.Text) >= d.size() || int.Parse(read_txtbox.Text) < 0)
                     MessageBox.Show("invalid index");
                 else
                 {
-                    MessageBox.Show("the element you looking for is: " + l.ElementAt(int.Parse(read_txtbox.Text)));
+                    MessageBox.Show("the element you looking for is: " + d.read_by_index(int.Parse(read_txtbox.Text)));
                 }
             }
         }
@@ -111,13 +146,41 @@ namespace CRUD_application
         private void read_list_btn_Click(object sender, EventArgs e)
         {
             list = string.Empty;
-            for (int i = 0; i < l.Count; i++)
+            for (int i = 0; i < d.size(); i++)
             {
-                list += l.ElementAt(i) + '\n';
+                list += d.read_by_index(i) + '\n';
             }
             read_richTextBox.Text = list;
         }
         //Ïå æÇÍÏ ãä Çåã ÇáÇÒÑÇÑ Ýí ÇáÈÑäÇãÌ æÙíÝÊå ÈÚÏ ãÇ ÇÚãá Úãáíå æÚÇæÒ ÇÑæÍ áÛíÑåÇ ÈíÎÝí Çí Úãáíå ßÇäÊ ÔÛÇáå æÈíÙåÑ ÇáÇÒÑÇÑ ÇáÇÓÇÓíÉ(ÇáßÑæÏ) Ýíí ÇáÕÝÍÉ
+        //åäÇ æÙíÝÉ ÇáÒÑÇÑ Ïå Çäå íÊÃßÏ Çáä ÇáãÓÊÎÏã ÏÎá ÞíãÉ ÇáÚäÕÑ Çááí ÚÇæÒ íÚãáå ÇÈÏíÊ æÈíÈÏáå ÈÇáÚäÕÑ ÇáÌÏíÏ
+        private void update_btn_Click(object sender, EventArgs e)
+        {
+            if (d.size() == 0)
+                MessageBox.Show("database is empty ):");
+            else
+            {
+                if (d.is_exist(update_txtBox_origin_value.Text))
+                {
+                    d.update(update_txtBox_origin_value.Text,update_txtBox_new_value.Text);
+                    update_txtBox_new_value.Text = string.Empty;
+                    update_txtBox_origin_value.Text = string.Empty;
+                }
+                else
+                    MessageBox.Show("the element you have entered does not exist the database ):");
+            }
+        }
+        //æÏå ÇáÒÑÇÑ ÇáãÓÄá Çäå íãÓÍ ÚäÕÑ ãÚíä ãä ÇááíÓÊ ÇáãÓÊÎÏã Çááí ÈíÏÎáå
+        private void delete_btn_Click(object sender, EventArgs e)
+        {
+            if (d.is_exist(delete_txtBox.Text))
+            {
+                d.delete(delete_txtBox.Text);
+                delete_txtBox.Text = string.Empty;
+            }
+            else
+                MessageBox.Show("the element you have entered does not exist the database ):");
+        }
         private void back_Click(object sender, EventArgs e)
         {
             create.Visible = true;
@@ -142,36 +205,8 @@ namespace CRUD_application
             delete_btn.Visible = false;
             delete_value_lbl.Visible = false;
             delete_txtBox.Visible = false;
-        }
-        //åäÇ æÙíÝÉ ÇáÒÑÇÑ Ïå Çäå íÊÃßÏ Çáä ÇáãÓÊÎÏã ÏÎá ÞíãÉ ÇáÚäÕÑ Çááí ÚÇæÒ íÚãáå ÇÈÏíÊ æÈíÈÏáå ÈÇáÚäÕÑ ÇáÌÏíÏ
-        private void update_btn_Click(object sender, EventArgs e)
-        {
-            if (l.Count == 0)
-                MessageBox.Show("the list is empty ):");
-            else
-            {
-                if (l.Contains(update_txtBox_origin_value.Text))
-                {
-                    int index = l.IndexOf(update_txtBox_origin_value.Text);
-                    l.Remove(update_txtBox_origin_value.Text);
-                    l.Insert(index, update_txtBox_new_value.Text);
-                    update_txtBox_new_value.Text = string.Empty;
-                    update_txtBox_origin_value.Text = string.Empty;
-                }
-                else
-                    MessageBox.Show("the element you have entered does not exist the list ):");
-            }
-        }
-        //æÏå ÇáÒÑÇÑ ÇáãÓÄá Çäå íãÓÍ ÚäÕÑ ãÚíä ãä ÇááíÓÊ ÇáãÓÊÎÏã Çááí ÈíÏÎáå
-        private void delete_btn_Click(object sender, EventArgs e)
-        {
-            if (l.Contains(delete_txtBox.Text))
-            {
-                l.Remove(delete_txtBox.Text);
-                delete_txtBox.Text = string.Empty;
-            }
-            else
-                MessageBox.Show("the element you have entered does not exist the list ):");
+            read_richTextBox.Text=string.Empty;
+            read_txtbox.Text=string.Empty;
         }
     }
 }
